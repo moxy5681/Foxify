d0a296162ad8fab9ab7ef839c403b8c5b0ef8536
moxy5681 <m.ttursunova@outlook.com>
2026-08-11 16:12:05 +0100

    finished fx forward in class SA

--- main.py
+++ main.py
@@ -66,3 +66,12 @@ class SelfAttention(nn.Module):
         q = q.view(B, T, self.n_heads, self.head_dim).transpose(1,2)
         k = k.view(B, T, self.n_heads, self.head_dim).transpose(1,2)
         v = v.view(B, T, self.n_heads, self.head_dim).transpose(1,2)
+
+        att = (q @ k.transpose(-2, -1)) / (self.head_dim ** 0.5)
+        mask = torch.tril(torch.ones(T, T)).view(1, 1, T, T)
+        att = att.masked_fill(mask == 0, float("-inf"))
+        att = F.softmax(att, dim = -1)
+
+        out = att @ v
+        out = out.transpose(1,2).contiguous().view(B, T, C)
+        return self.proj(out)
